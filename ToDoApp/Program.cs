58ea81953e5592566eca4ee46dc68e4110b28e7a
TheQuantumMoon <114161742+TheQuantumMoon@users.noreply.github.com>
2026-03-12 18:56:54 +0100

    Add project files.

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoApp {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+
+            // Dag Github! -Arnout
+
+            Console.WriteLine("\nEINDE PROGRAMMA");
+            Console.ReadKey();
+        }
+    }
+}
