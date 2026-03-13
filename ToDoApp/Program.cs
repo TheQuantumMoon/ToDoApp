a1e759045e25cd5c1516431dffecb05405a98edc
TheQuantumMoon <114161742+TheQuantumMoon@users.noreply.github.com>
2026-03-13 16:44:02 +0100

    Created a system for reading and writing to the mock database "database.txt"

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,9 +1,52 @@
 ﻿namespace ToDoApp {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            /* --------------------Regels--------------------
+             * Minder dan 250 lijnen
+             * Alles in het Nederlands
+             * Geen methods maken
+             * Informatie bijhouden in paralelle arrays
+             * Lijsten opslaan in een tekst bestand, ga er van uit dat het altijd correct is opgeslagen
+             * Regelatig comitten naar de repo
+             */
 
-            // Dag Github! -Arnout
+            string[] databank;
+            if (File.Exists("databank.txt") && File.ReadAllLines("databank.txt").Length >= 3) {
+                databank = File.ReadAllLines("databank.txt");
+            }
+            else databank = ["", "", ""];
+
+            string[] takenBeschrijving = databank[0].Split("|");
+            string[] takenDeadline = databank[1].Split("|");
+            string[] takenVoltooid = databank[2].Split("|");
+
+            Console.WriteLine("Taken:"); // Toont een taak als er een beschrijving is, maar geen deadline of voltooidtijd
+            for (int i = 0; i < takenBeschrijving.Length; i++) {
+                if (takenBeschrijving[i] != "" && takenDeadline[i] == "" && takenVoltooid[i] == "") {
+                    Console.WriteLine(" *   " + takenBeschrijving[i]);
+                }
+            }
+            Console.WriteLine("\nTaken met deadline:"); // Toont een deadline taak als er een beschrijving is, een deadline en geen voltooidtijd
+            for (int i = 0; i < takenBeschrijving.Length; i++) {
+                if (takenBeschrijving[i] != "" && takenDeadline[i] != "" && takenVoltooid[i] == "") {
+                    Console.WriteLine($" *   deadline: {takenDeadline[i]} | {takenBeschrijving[i]}");
+                }
+            }
+            Console.WriteLine("\nVoltooide taken:"); // Toont een voltooide taak als er een beschrijving is, eventueel een deadline en een voltooidtijd
+            for (int i = 0; i < takenBeschrijving.Length; i++) {
+                if (takenBeschrijving[i] != "" && takenVoltooid[i] != "" && takenDeadline[i] == "") {
+                    Console.WriteLine($" *   voltooid op: {takenVoltooid[i]} | {takenBeschrijving[i]}");
+                }
+                else if (takenVoltooid[i] != "" && takenDeadline[i] != "") {
+                    Console.WriteLine($" *   voltooid op: {takenVoltooid[i]} | deadline: {takenDeadline[i]} | {takenBeschrijving[i]}");
+                }
+            }
+
+            databank[0] = string.Join("|", takenBeschrijving);
+            databank[1] = string.Join("|", takenDeadline);
+            databank[2] = string.Join("|", takenVoltooid);
+
+            File.WriteAllLines("databank.txt", databank);
 
             Console.WriteLine("\nEINDE PROGRAMMA");
             Console.ReadKey();
